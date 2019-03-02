3cd67dc3b069b33222cd5e5db73f05ca4fa16755
manish <ms3236@cornell.edu>
2019-03-02 17:11:03 -0500

    Refactor

--- preprocessing.py
+++ preprocessing.py
@@ -2,11 +2,11 @@ import csv
 
 arr = []
 
-with open('all_uisc_clean.csv') as uni_data:
+with open('datasets/all_uisc_clean.csv') as uni_data:
     csv_reader = csv.reader(uni_data, delimiter=",")
     college_set = set()
 
-    with open('cornellAdmissions.csv', mode='w', newline='') as colleges:
+    with open('datasets/cornellAdmissions.csv', mode='w', newline='') as colleges:
         employee_writer = csv.writer(colleges, delimiter=',')
         for row in csv_reader:
             if "Cornell" in row[2] and row[3] != "" and row[6] != "" and row[10] != "" and row[11] != "" and row[12] != "" and row[13] != "":
