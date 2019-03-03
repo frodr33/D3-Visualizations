81cf34a6f39bac491bba30dc732dd65659847c56
Frank <fsr32@cornell.edu>
2019-03-02 21:48:41 -0500

    more work on sankey

--- preprocessing.py
+++ preprocessing.py
@@ -8,7 +8,7 @@ with open('datasets/all_uisc_clean.csv') as uni_data:
 
     with open('datasets/cornellAdmissions.csv', mode='w', newline='') as colleges:
         employee_writer = csv.writer(colleges, delimiter=',')
-        employee_writer.writerow(["College", "Major","Accepted", "GPA", "GRE Verbal", "GRE Quant", "GRE writing"])          
+        employee_writer.writerow(["College", "Major","Accepted", "GPA", "GRE_Verbal", "GRE_Quant", "GRE_Writing"])          
         for row in csv_reader:
             if ("Cornell" in row[2] and row[3] != "" and row[6] != "" and row[10] != "" and row[11] != "" and row[12] != "" and row[13] != "" 
                 and float(row[11]) < 170.0 and float(row[12]) < 170.0 and float(row[13]) < 6.0):
