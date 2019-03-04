749fffc8fc6402e437633834291a2884bb1e620a
Frank <fsr32@cornell.edu>
2019-03-04 17:50:38 -0500

    preprocessing sorted

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,7 @@ import csv
 
 with open('datasets/all_uisc_clean.csv') as uni_data:
     csv_reader = csv.reader(uni_data, delimiter=",")
+    schoolRows = []
     with open('datasets/cornellAdmissions.csv', mode='w', newline='') as colleges:
         employee_writer = csv.writer(colleges, delimiter=',')
         employee_writer.writerow(["College", "Major","Accepted", "GPA", "GRE_Verbal", "GRE_Quant", "GRE_Writing"])          
@@ -17,4 +18,7 @@ with open('datasets/all_uisc_clean.csv') as uni_data:
 
             if (school and row[3] != "" and row[6] != "" and row[10] != "" and row[11] != "" and row[12] != "" and row[13] != "" 
                 and float(row[11]) < 170.0 and float(row[12]) < 170.0 and float(row[13]) < 6.0):
-                employee_writer.writerow([school, row[3], row[6], row[10], row[11], row[12], row[13]])          
+                schoolRows.append([school, row[3], row[6], row[10], row[11], row[12], row[13]])  
+
+        csv.writer(colleges, delimiter=";")
+        employee_writer.writerows(sorted(schoolRows))     
